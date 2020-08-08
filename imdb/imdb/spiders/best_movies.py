ab8339d5680d38cf28a42d05545dcc4fe06b92aa
Derek Dyer <derekdyer02@gmail.com>
2020-08-07 21:22:01 -0700

    added imdb to repo

---
+++ imdb/imdb/spiders/best_movies.py
@@ -0,0 +1,36 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = 'best_movies'
+    allowed_domains = ['www.imdb.com']
+
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[2]"), process_request='set_user_agent')
+    )
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc', headers={
+            'user-agent': self.user_agent 
+        })
+
+    def set_user_agent(self, request, spider):
+        request.headers['user-agent'] = self.user_agent
+        return request
+
+
+    def parse_item(self, response):
+        yield {
+            'title': response.xpath("//div[@class='title_wrapper']//h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration': response.xpath("normalize-space((//time)[1]/text())").get(),
+            'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url': response.url,
+            'user-agent': response.request.headers['user-agent'].decode('utf-8')
+        }
